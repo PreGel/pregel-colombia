95f575ecf338fc2ffaac7121d2bfc97c5e2e5444
makrmk <markwerks@gmail.com>
2019-06-20 14:45:35 -0400

    files from live

---
+++ footer.php
@@ -0,0 +1,125 @@
+    <footer class="footer" role="contentinfo">
+    	<div class="grid-container">
+    		<div id="inner-footer" class="grid-x">
+    			<div class="large-4 medium-6 small-12 cell">
+    				<div class="call-to-action-row">
+    					<h5>Suscríbase a Nuestro Boletín Informativo</h5>
+    					<button id="footer-button" class="button" value="Sign Up" data-open="footer-contact">Haga clic aquí</button>
+    					<div class="reveal" id="footer-contact" data-reveal>
+    						<iframe src="https://go.pardot.com/l/95432/2017-07-26/2t7w5g" width="100%" height="500" type="text/html" frameborder="0" allowTransparency="true" style="border: 0"></iframe>		   
+    					</div>
+    				</div>
+    				<div id="subsidiaries-row">	
+    					<a href="https://pregelcolombia.com/ubicaciones/" class="expanded button hollow"> 
+    						<!--  <div id="global-img"></div> -->
+    						Ver todas las subsidiarias de PreGel
+    					</a>			 
+    				</div>
+    			</div>			    
+
+    		<div class="large-4 medium-6 small-12 cell text-center">
+    			<ul class="footer-social text-center">
+    				<li>
+    					<a href="https://www.facebook.com/pages/PreGel-COLOMBIA/1518484041711304" target="_blank">
+    						<img src="<?php echo get_template_directory_uri(); ?>/assets/images/facebook.png" alt="Facebook" />
+    					</a>
+    				</li>
+    				<li>
+    					<a href="http://www.linkedin.com/company/482070" target="_blank">
+    						<img src="<?php echo get_template_directory_uri(); ?>/assets/images/linkedin.png" alt="LinkedIn" />
+    					</a>
+    				</li>
+    				<li> 
+    					<a href="https://www.pinterest.com/pregelamerica/pregel-colombia/" target="_blank">
+    						<img src="<?php echo get_template_directory_uri(); ?>/assets/images/pinterest.png" alt="Pinterest" />
+    					</a>
+    				</li>
+    				<li>
+    					<a href="http://instagram.com/pregelcolombia" target="_blank">
+    						<img src="<?php echo get_template_directory_uri(); ?>/assets/images/instagram.png" alt="Instagram" />
+    					</a>
+    				</li>
+    			</ul>
+    			<div><a href="https://api.whatsapp.com/send?phone=573155872420"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/WhatsApp_Logo_1.png" alt="whatsapp logo"/><strong> +57 315 5872420</strong></a></div>
+    		</div>
+
+    		<div id="right-footer-links" class="large-4 medium-6 small-12 cell">
+    			<ul class="footer-links">
+    				<li class="text-left">
+    					<a href="https://pregelcolombia.com/oportunidades-de-empleo/">Oportunidades de Empleo</a>
+    				</li>
+    				<li class="text-left">
+    					<a href="https://pregelcolombia.com/contactanos/">Contáctenos</a>
+    				</li>
+    				<li class="text-left">
+    					<a href="https://pregelcolombia.com/politica-de-privacidad/">Política de Privacidad</a>
+    				</li>
+    				<li class="text-left">
+    					<a href="https://pregelcolombia.com/condiciones-de-uso/">Condiciones de Uso</a>
+    				</li>
+    				<li class="text-left">
+    					<a href="https://pregelcolombia.com/politicas-de-envios/">Politica de Envíos</a>
+    				</li>	
+    				<li class="text-left">
+    					<button class="hollow button" href="https://pregelcolombia.com/pdf/Politica%20de%20Tratamiento%20de%20Datos%20Personales%20PREGEL%20COLOMBIA%20SAS.pdf">Política de Tratamiento de Datos Personales</button>
+    				</li>							
+    			</ul>
+    		</div>
+    	</div><!-- end #inner-footer -->
+    </div> <!-- end grid-container -->
+</footer> <!-- end .footer -->
+
+<div id="bottom-footer" class="row-expanded">
+	<div class="grid-x">
+		<div id="footer-contact-info" class="large-4 small-12 cell">
+			<strong><p class="large-12 medium-12 small-12 text-center cell">Sede Central Bogotá</p></strong>
+			<p class="large-12 medium-12 small-12 text-center cell">Carrera 1era No. 4-02 Interior 3</p>
+			<p class="large-12 medium-12 small-12 text-center cell">Parque Industrial K2</p>
+			<p class="large-12 medium-12 small-12 text-center cell">Municipio de Chia, Departamento de Cundinamarca</p>
+			<p class="large-12 medium-12 small-12 text-center cell">Código postal 250001</p>
+			<p class="large-12 medium-12 small-12 text-center cell"><strong>Teléfono: 57 1 884 4421</strong></p>
+		</div>
+		<div id="footer-contact-info" class="large-4 small-12 cell">
+			<strong><p class="large-12 medium-12 small-12 text-center cell">Medellín</p></strong>
+			<p class="large-12 medium-12 small-12 text-center cell">Calle 11 No. 43b-50 </p>
+			<p class="large-12 medium-12 small-12 text-center cell">Centro Empresarial Calle 11</p>
+			<p class="large-12 medium-12 small-12 text-center cell">Local 108</p>
+			<p class="large-12 medium-12 small-12 text-center cell">Medellín, Antioquia</p>
+			<p class="large-12 medium-12 small-12 text-center cell"><strong>Teléfono: 57 4 311 9162</strong></p>
+		</div>
+		<div id="footer-contact-info" class="large-4 small-12 cell">
+			<strong><p class="large-12 medium-12 small-12 text-center cell">Cali</p></strong>
+			<p class="large-12 medium-12 small-12 text-center cell">Av 6A N # 23-93 Local 1</p>
+			<p class="large-12 medium-12 small-12 text-center cell">Barrio Santa Mónica Residencial</p>
+			<p class="large-12 medium-12 small-12 text-center cell">Cali, Valle del Cauca</p>
+			<p class="large-12 medium-12 small-12 text-center cell"><strong>Teléfono: 57 2 399 5492</strong></p>
+		</div>
+		<div class="large-12 medium-12 small-12 text-center cell">
+			<p id="copyright" class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. Todos los Derechos Reservados.</p>
+		</div>
+	</div>
+</div>
+
+
+</div>  <!-- end .main-content -->
+</div> <!-- end .off-canvas-wrapper-inner -->
+</div> <!-- end .off-canvas-wrapper -->
+
+<?php wp_footer(); ?>
+
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+	var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+	s1.async=true;
+	s1.src='https://embed.tawk.to/5b69e977e21878736ba2b6db/default';
+	s1.charset='UTF-8';
+	s1.setAttribute('crossorigin','*');
+	s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+
+</body>
+</html> <!-- end page -->
---
+++ template-faq.php
@@ -0,0 +1,170 @@
+<?php
+/*
+Template Name: FAQ
+*/
+?>
+<?php get_header(); ?>
+<script>
+jQuery( document ).ready( function( $ ) {
+		    // $() will work as an alias for jQuery() inside of this function
+		    /*FAQ toggle boxes*/
+		    jQuery( "#faq-1" ).click(function() {
+		    	jQuery( "#faq-toggle-1" ).toggle( "slow", function() {
+		    	});
+		    	return false;
+		    });
+		    jQuery( "#faq-2" ).click(function() {
+		    	jQuery( "#faq-toggle-2" ).toggle( "slow", function() {
+		    	});
+		    	return false;
+		    });
+		    jQuery( "#faq-3" ).click(function() {
+		    	jQuery( "#faq-toggle-3" ).toggle( "slow", function() {
+		    	});
+		    	return false;
+		    });
+		    jQuery( "#faq-4" ).click(function() {
+		    	jQuery( "#faq-toggle-4" ).toggle( "slow", function() {
+		    	});
+		    	return false;
+		    });
+		} );
+</script>
+<div id="content" class="pg-about-content">
+	
+	<!-- Title Banner of Page with Big Image -->
+	<div id="pg-about-banner-container">
+		<div id="pg-about-banner"></div>
+	</div>
+	<div id="inner-support-content">
+
+		<div id="title-banner" class="large-centered columns">
+			<h1 class="text-center">Preguntas Frecuentes</h1>							
+		</div>
+
+		<div class="grid-container">
+			<div class="grid-x grid-margin-x">
+				
+				<div id="main" class="large-8 medium-12 small-12 cell" role="main">
+					<p>Consulte nuestra guía de preguntas frecuentes donde podrá encontrar las respuestas que necesita. Desde información básica de nuestros productos hasta aspectos técnicos de heladería. PreGel mantiene un compromiso categórico con la capacitación de sus clientes y del rubro, por lo cual ponemos a su alcance diferentes recursos para que incremente su conocimiento y técnica. Si usted tiene alguna otra pregunta, no dude en contactarnos a través del correo electrónico <a href="mailto:info@pregelcolombia.com">info@pregelcolombia.com.</a></p>
+
+					<a id="faq-1" class="expanded button faq-a" href="">PRODUCTOS PREGEL Y PREGUNTAS GENERALES SOBRE SU PREPARACIÓN</a>
+					<div id="faq-toggle-1">	
+						<h3>¿Cuáles son los ingredientes básicos del Gelato?</h3>
+						<p>El Gelato está compuesto por un ingrediente básico líquido y otro sólido, estabilizantes, emulsionantes, ingredientes de sabor, aire y otros ingredientes.</p>
+						<p>Por ejemplo:</p>
+						<div style="text-align: left; padding-left: 100px;">
+							<ul>
+								<li><em>Ingredientes básicos líquidos</em>: agua, leche, etc.</li>
+								<li><em>Ingredientes básicos sólidos</em>: azúcares, grasas, MSNFs</li>
+								<li><em>Estabilizantes</em>: Goma guar, goma garrofín, etc.</li>
+								<li><em>Emulsionantes</em>: Mono y diglicéridos</li>
+								<li><em>Ingredientes de Sabor</em>: Pastas, polvos</li>
+								<li><em>Aire</em></li>
+								<li><em>Otros Ingredientes</em>: Sales minerales, productos de cacao, frutos secos, toppings y rellenos</li>
+							</ul>
+						</div>
+
+						<h3>¿Qué es una base?</h3> 
+						<p>Las Bases de PreGel son ingredientes esenciales en polvo perfectamente equilibrados.  
+							Las mismas tienen como función principal el optimizar el sabor y textura del gelato, así como facilitar su producción.  La Base es fundamental para crear una estructura y textura que permitan la cremosidad y suavidad deseadas de las que se enorgullece PreGel.</p>
+
+							<h3>¿Qué es un estabilizante?</h3>
+							<p>Existen dos tipos de estabilizantes utilizados en el gelato. Uno proviene de fuentes animales y el más comúnmente utilizado es la gelatina. El otro estabilizante es una fuente vegetal como carragenano, goma de guar y Goma de garrofín. Los estabilizantes se utilizan en el gelato por una variedad de razones:  incrementan la viscosidad, mejoran la suavidad del cuerpo del producto e impiden que se formen cristales de hielo y que se derrita.</p>
+
+							<h3>¿Qué es el Arabeschi®?</h3> 
+							<p>El Arabeschi® es un topping y relleno para el gelato. Como topping se utiliza para decorar y hacer más llamativo el gelato en tarrinas y bandejas, así como los semifreddo. También se emplea como relleno para darle textura y sabor adicional al gelato. Entre algunos de nuestros mejores Arabeschi® se encuentran el Pino Pinguino®, el Crocante de Café y diferentes tipos de Frutas Confitadas.</p>
+
+							<h3>¿Cuáles son los ingredientes básicos para elaborar gelato?</h3> 
+							<p>En la explicación más básica, el gelato se hace mezclando leche o agua, azúcar y otros ingredientes naturales (como emulsionantes y estabilizantes) que integran la base, luego se agregan nuestros distintivos sabores de crema o fruta.</p>
+
+							<h3>¿Cuál es la diferencia entre el proceso frío y el caliente?</h3>
+							<p>El proceso caliente requiere de pasteurización, mientras que el proceso frío no. Para aprender acerca de estos procesos, lo invitamos a asistir a una de nuestras clases de capacitación.</p> 
+
+							<h3>¿Cuál es la cantidad de saborizante que debo utilizar?</h3>
+							<p>Todo depende de la receta que elabore, cada saborizante tienen instrucciones en su empaque de una receta estándar. Si tiene cualquier pregunta no dude en consultar a su Asesor Comercial o nuestro Departamento de Servicio al Cliente.</p>
+
+							<h3>¿Durante cuánto tiempo puedo guardar mi gelato en la vitrina?</h3>
+							<p>Esta respuesta variará ligeramente dependiendo de diferentes factores durante la preparación, pero por lo general entre 3 y 5 días, siempre y cuando guarde su gelato en el congelador por la noche. Quizá consiga también una mayor vida útil si prepara su gelato con el proceso caliente. Tenga en cuenta que la forma preferida de servir el gelato es cuando se encuentra en su estado más fresco.</p>
+
+							<h3>¿Cuál es la temperatura para el almacenaje?</h3> 
+							<p>La temperatura para almacenar en el congelador debe de ser de aproximadamente -17.7 grados centígrados.</p>
+
+							<h3>¿A qué temperatura tiene que estar el gelato en cuanto sale del mantecador?</h3> 
+							<p>Entre -7.7 y -5 grados centígrados. En este punto, sólo entre el 50% y el 60% del agua está congelada.</p>
+
+							<h3>¿Cómo se presenta el gelato?</h3> 
+							<p>Por lo general en contenedores de 5 litros. Sale del congelador discontinuo fácilmente, sin necesidad de presionar o empujarlo.  El resultado debe ser armonioso con muchas ondas y rizos.</p>
+
+							<h3>¿Cómo se sirve el gelato?</h3> 
+							<p>Aunque con el helado se suelen utilizar palas, el gelato se sirve con espátulas, que le permiten adoptar una forma más artesanal.</p>
+
+							<h3>¿Decoración del gelato?</h3> 
+							<p>Sin duda, el gelato es considerado un postre artesanal, lo cual significa que la decoración y presentación del mismo son tan importantes como su sabor y textura. Dichos elementos forman parte esencial de la experiencia integral de disfrutar un gelato. El Arabeschi® y los Toppings de PreGel le agregan no solo sabor y textura al gelato, sino también lo hacen más apetitivos a la vista del consumidor.</p>  
+
+							<h3>¿Durante cuánto tiempo durarán mis productos PreGel?</h3> 
+							<p>La mayoría de los productos PreGel tienen una vida útil de tres años a partir del momento en que son producidos; el que se hayan abierto no afecta su vida útil. Sin embargo, recuerde guardar los productos bien sellados y no los almacene en temperaturas extremas, esto puede acortar su vida útil. La fecha de caducidad se indica en el empaque en la forma europea, día/mes/año. ¿ Cuánto tiempo durarán mis productos? Atencion a Clientes-Servicio al Cliente.</p>
+
+							<h3>¿Qué debo hacer cuando el aceite se separa en las Pastas, Arabeschi® o los Toppings?</h3>
+							<p>Simplemente mézclelos, en pocos segundos recuperaran su textura original. Y agite los toppings. Le recomendamos los mezcle de vez en cuando para evitar la separación.</p> 
+
+							<h3>¿Cuál es el contenido nutricional de mi producto?</h3> 
+							<p>PreGel sólo puede proporcionar información nutricional sobre los productos no acabados que produce. Si quiere evaluar los contenidos nutricionales del producto acabado que usted elabora, le recomendamos que se asocie con un laboratorio local de pruebas que pueda proporcionarle con exactitud la información nutricional sobre su producto.</p>
+
+							<h3>¿Qué tipo de información proporcionar para ayudarme a empezar un negocio exitoso de gelato?</h3>
+							<p>Incorporadas al currículo de capacitación, PreGel ofrece varias herramientas diseñadas para ayudarlo a lanzar un negocio de gelato exitoso. Nuestra capacitación puede proporcionar instrucciones cuando está diseñando su configuración de precios, estructurando su inversión inicial, estableciendo lineamientos generales para los empleados y mucho más.</p>
+
+						</div>
+
+	<!-- <a id="faq-2" class="expanded button faq-a" href="">PREGUNTAS SOBRE EDUCACIÓN PROFESIONAL EN EL CENTRO DE CAPACITACIÓN DE PREGEL</a>
+     <div id="faq-toggle-2">
+		<h3>¿Por qué se ofrecen sin costo las Clases de Capacitación de PreGel?</h3>
+		En PreGel, creemos que la educación es el pegamento que une la pasión de nuestros clientes con nuestros ingredientes. En EEUU, el Gelato apenas está empezando a surgir en el mercado de la corriente principal, y PreGel está dedicado a asegurarse que todos los estadounidenses o sólo llegan a experimentar el Gelato, sino que reciben el Gelato de la mejor calidad Gelato. Por lo tanto, en PreGel creemos que es fundamental educar a todos nuestros clientes con nuestros procesos innovadores, recetas precisas y experiencia artesanal para ayudarlos a proporcionar un producto acabado sin igual. Esta dedicación a nuestros productos y clientes se refleja en nuestra capacidad para ofrecer clases gratuitas.
+
+		<h3>¿Cuándo y dónde son las capacitaciones? ¿Hacen capacitaciones en el lugar del cliente?</h3>
+		Haga clic aquí para ver nuestras ofertas actuales de clases en nuestra ubicación en Concord. Además, vuelva a visitar periódicamente la página por si aparecen también capacitaciones fuera del sitio. Además, somos muy flexibles y queremos ayudar a nuestros clientes, así que llamen a nuestro Centro de Capacitación de Educación Profesional para programar visitas especiales o clases a la medida en el (81)821-20-108
+
+		<h3>¿Qué tipo de información pueden proporcionar para ayudarme a empezar un negocio exitoso de Gelato?</h3>
+		Incorporadas al currículo de capacitación, PreGel ofrece varias herramientas diseñadas para ayudarlo a lanzar un exitoso negocio de Gelato. Nuestra capacitación puede proporcionar instrucciones cuando está diseñando su configuración de precios, estructurando su inversión inicial, estableciendo lineamientos generales para los empleados y mucho más.
+
+		<h3>¿Es el Gelato un negocio para llevar?</h3>
+		PreGel por lo general promueve el Gelato como una experiencia y actividad social para disfrutar con la familia y los amigos fuera del hogar. Sin embargo, sí vendemos contenedores para llevar para que sus clientes lo lleven a casa para compartir con la familia.
+	 
+	</div> -->
+
+    <!-- <a id="faq-3" class="expanded button faq-a" href="">PREGUNTAS SOBRE DISTRIBUCIÓN Y FACTURACIÓN</a>
+     <div id="faq-toggle-3">
+		<h3>¿Dónde está mi envío?</h3>
+		La forma más fácil de seguir su envío es ubicar el número de control en su factura, la que le enviamos tanto por correo ordinario como por correo electrónico. Luego, vaya al sitio en Internet de la empresa de transporte (UPS, Con-Way o Bullet). En cuanto esté en el sitio, debe haber una sección en la que puede introducir su número de control y el sitio en Internet debe darle los datos de cómo avanza su pedido. Si no puede acceder a Internet o tiene problemas, por favor llámenos directamente al (81)821.20.108 y asegúrese que tiene a la mano su número de control.
+
+		<h3>¿Cómo hago mi primer pedido? </h3>
+		Nuestros representantes de venta están divididos por regiones, así que haga clic aquí para decidir cuál de nuestros Gerentes de Ventas representa a su región. Luego póngase en contacto con ellos por teléfono o por correo electrónico y les complacerá ayudarlo a hacer su primer pedido.
+
+		<h3>¿Qué tengo que hacer para conseguir un crédito/conocer los términos?</h3>
+		Con el fin de solicitar un crédito con PreGel, se deben comprar productos por un valor mínimo de $6,000.00 dólares. No tiene que ser un pedido inicial, sino que puede ser acumulado. Al llegar a los $6,000.00 dólares en ventas, un miembro del Equipo de Servicios Especiales le enviará por fax, correo electrónico o correo ordinario una solicitud de crédito. La solicitud DEBE completarse en su TOTALIDAD. (Nota: La sección de Referencia Comercial tiene que ser completada, incluyendo TODOS los campos: nombre del vendedor, dirección del vendedor, teléfono del vendedor y fax del vendedor. Si tiene un número de cuenta con el vendedor, inclúyalo también por favor.) Tras completar la solicitud, envíela de vuelta a PreGel y procederemos a procesar su solicitud solicitando su historial crediticio (puede llevar de 5 a 10 días). PreGel revisará entonces toda a información e historial proporcionado y se le informará de la decisión. Todo este proceso dura aproximadamente 14 días.
+		
+	</div> -->
+
+	<a id="faq-4" class="expanded button faq-a" href="">OTRAS PREGUNTAS</a>
+	<div id="faq-toggle-4">
+
+		<h3>¿Cuáles son los sabores más populares de PreGel?</h3>
+		<p>Para tener una lista de los sabores más populares, PreGel toma en cuenta las ventas de estos últimos años. Los sabores más populares (en ningún orden en particular) han sido: Yoggi 30®, Fortefrutto® de Frutos del Bosque, Pasta Tradicional de Avellana Piemonte, Pasta Tradicional de Cacaopat, Pasta Tradicional de Pistacho, Pasta Tradicional de Tiramisú,   Arabeschi® de Otto Caramel Biscotto, Arabeschi® Gran Stracciatella Reale, Arabeschi® Pepita, entre otros. </p>
+		
+	</div>
+
+</div>
+
+
+<?php get_sidebar('help'); ?>
+
+</div>
+</div>
+
+
+</div>
+
+</div> <!-- end #inner-content -->
+</div> <!-- end #content -->
+
+<?php get_footer(); ?>
